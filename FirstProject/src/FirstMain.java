e22315726f319957afe04103801df7c40b21c8dc
sagar <sagar@DESKTOP-DR611BD>
2017-12-07 12:01:04 +0530

    First Commit

---
+++ FirstProject/src/FirstMain.java
@@ -0,0 +1,11 @@
+
+public class FirstMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Test");
+		System.out.println("Test2");
+
+	}
+
+}
